ed23b820c8af54b949aa35790e396d7bd13ed563
Gregor Harlan <gregor.harlan@yakamara.de>
2015-07-06 10:40:10 +0200

    ModelExtension: Besserer Wert für empty_value

--- Form/Extension/ModelExtension.php
+++ Form/Extension/ModelExtension.php
@@ -3,14 +3,29 @@
 namespace Yakamara\CommonBundle\Form\Extension;
 
 use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class ModelExtension extends AbstractTypeExtension
 {
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'choice_translation_domain' => false,
+            'empty_value' => function (Options $options) {
+                if (!$options['multiple'] && !$options['expanded']) {
+                    return $this->translator->trans('select.empty_value');
+                }
+                return false;
+            },
         ]);
     }
 
