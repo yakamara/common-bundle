b62e11884d7ee52144af13d5eedda4246d50cc6f
Gregor Harlan <mail@gh01.de>
2016-01-25 12:41:24 +0100

    Update für Symfony 2.8

--- YakamaraCommonBundle.php
+++ YakamaraCommonBundle.php
@@ -4,14 +4,11 @@ namespace Yakamara\CommonBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Yakamara\CommonBundle\DependencyInjection\Compiler\AddSecurityVotersPass;
 
 class YakamaraCommonBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
-
-        $container->addCompilerPass(new AddSecurityVotersPass());
     }
 }
