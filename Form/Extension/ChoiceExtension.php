89ab2c528e5411f3e2814a89596e20077840ef58
Gregor Harlan <mail@gh01.de>
2016-04-04 12:26:01 +0200

    placeholder allgemein für Choice-Felder

--- Form/Extension/ChoiceExtension.php
+++ Form/Extension/ChoiceExtension.php
@@ -4,20 +4,26 @@ namespace Yakamara\CommonBundle\Form\Extension;
 
 use Propel\Bundle\PropelBundle\Form\Type\ModelType;
 use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ModelExtension extends AbstractTypeExtension
+class ChoiceExtension extends AbstractTypeExtension
 {
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'choice_translation_domain' => false,
+            'placeholder' => function (Options $options) {
+                if (!$options['multiple'] && !$options['expanded']) {
+                    return 'select.placeholder';
+                }
+                return false;
+            },
         ]);
     }
 
     public function getExtendedType()
     {
-        return ModelType::class;
+        return ChoiceType::class;
     }
 }
