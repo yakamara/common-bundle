ad56f3df5a8e221462edbe2856e628e04fead2dc
Gregor Harlan <mail@gh01.de>
2017-06-19 15:34:12 +0200

    BaseModelFakerPass
    Zur Compile-Time des Containers existieren die Base-Models teilweise noch nicht, müssen daher gefaked werden, damit es hier nicht abbricht:
    
    https://github.com/symfony/symfony/blob/3bbb657560615b80c8a9b3b6da8602349bd3fbd9/src/Symfony/Component/HttpKernel/DependencyInjection/RegisterControllerArgumentLocatorsPass.php#L136-L143

---
+++ DependencyInjection/Compiler/BaseModelFakerPass.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the common-bundle package.
+ *
+ * (c) Yakamara Media GmbH & Co. KG
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Yakamara\CommonBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class BaseModelFakerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        spl_autoload_register([$this, 'loadClass'], true, true);
+    }
+
+    private function loadClass($class)
+    {
+        if ('AppBundle\\Model\\' !== substr($class, 0, 16)) {
+            return;
+        }
+
+        $class = substr($class, 16);
+        $dir = __DIR__.'/../../../../../src/Model';
+
+        if (file_exists($dir.'/Base/'.$class.'.php') || !file_exists($dir.'/'.$class.'.php')) {
+            return;
+        }
+
+        eval('namespace AppBundle\\Model\\Base; class '.$class.' {}');
+
+        include $dir.'/'.$class.'.php';
+    }
+}
--- YakamaraCommonBundle.php
+++ YakamaraCommonBundle.php
@@ -11,13 +11,17 @@
 
 namespace Yakamara\CommonBundle;
 
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Yakamara\CommonBundle\DependencyInjection\Compiler\BaseModelFakerPass;
 
 class YakamaraCommonBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
+
+        $container->addCompilerPass(new BaseModelFakerPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 100);
     }
 }
