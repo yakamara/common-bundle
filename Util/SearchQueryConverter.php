b25bc3cf3f0880a11ca16f7ef6ba9ddd772f6574
Gregor Harlan <mail@gh01.de>
2017-08-24 15:38:39 +0200

    SearchQueryConverter Klasse

---
+++ Util/SearchQueryConverter.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the common-bundle package.
+ *
+ * (c) Yakamara Media GmbH & Co. KG
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Yakamara\CommonBundle\Util;
+
+class SearchQueryConverter
+{
+    public static function convert(string $query): ?string
+    {
+        $parts = [];
+        foreach (str_getcsv($query, ' ') as $part) {
+            $op = '-' === $part[0] ? '-' : '+';
+            $part = ltrim($part, '+- ');
+            $part = str_replace(['"', '+', '-', '(', ')'], '', $part);
+
+            if (mb_strlen($part) <= 2) {
+                continue;
+            }
+
+            if (preg_match('/[ @*]/', $part)) {
+                $part = '"'.$part.'"';
+            } else {
+                $part .= '*';
+            }
+
+            $parts[] = $op.$part;
+        }
+
+        if (!$parts) {
+            return null;
+        }
+
+        return implode(' ', $parts);
+    }
+}
